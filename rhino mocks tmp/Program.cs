8ee47752596b89aae824c46d48b408960e307bfb
Petr Onderka <gsvick@gmail.com>
2014-03-12 12:29:58 +0100

    firs commit

---
+++ rhino mocks tmp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Rhino.Mocks.MethodRecorders;
+
+namespace rhino_mocks_tmp
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+    var foo = MockRepository.GenerateMock<IFoo>();
+    foo.Expect(x => x.GetValue(42)).Return("forty two");
+
+    // in real test, this would call the tested method
+    var value = foo.GetValue(42);
+
+    Assert.AreEqual("forty two", value);
+        }
+    }
+
+    public interface IFoo
+    {
+        string GetValue(int number);
+        string GetValue2(int number);
+
+        string Name { get; set; }
+    }
+}
