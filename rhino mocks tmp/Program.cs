09177ffcb926b8494e7c94cfb2ee596c2fb9b086
Petr Onderka <gsvick@gmail.com>
2014-03-12 13:28:04 +0100

    update

--- rhino mocks tmp/Program.cs
+++ rhino mocks tmp/Program.cs
@@ -1,33 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Rhino.Mocks;
-using Rhino.Mocks.MethodRecorders;
 
 namespace rhino_mocks_tmp
 {
     class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-    var foo = MockRepository.GenerateMock<IFoo>();
-    foo.Expect(x => x.GetValue(42)).Return("forty two");
+            var foo = MockRepository.GenerateStrictMock<IFoo>();
+            foo.Expect(x => x.GetValue(42)).Return("forty two");
 
-    // in real test, this would call the tested method
-    var value = foo.GetValue(42);
-
-    Assert.AreEqual("forty two", value);
+            var value = foo.GetValue(42);
+            
+            Assert.AreEqual("forty two", value);
+            foo.VerifyAllExpectations();
         }
     }
 
     public interface IFoo
     {
         string GetValue(int number);
-        string GetValue2(int number);
-
-        string Name { get; set; }
     }
 }
